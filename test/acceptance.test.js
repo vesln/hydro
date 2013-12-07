99f3ce556cb82c74d8b09d041e82b6f550b53e99
Veselin Todorov <veselin@veselin.bg>
2013-12-07 15:07:31 +0200

    Get rid of Result

--- test/acceptance.test.js
+++ test/acceptance.test.js
@@ -1,43 +1,25 @@
-var Hydro = require('..');
-var fixtures = require('path').join(__dirname, 'fixtures');
+var run = require('./support/run');
 
 suite('acceptance', function() {
   test('passing tests', function(done) {
-    run('passing.js', function(root) {
-      root.result.passed.length.should.eq(3);
+    run('passing.js', function(result) {
+      result.passed.should.eq(3);
       done();
     });
   });
 
   test('failing tests', function(done) {
-    run('failing.js', function(root) {
-      root.result.failed.length.should.eq(5);
+    run('failing.js', function(result) {
+      result.failed.should.eq(5);
       done();
     });
   });
 
   test('skipped tests', function(done) {
-    run('skipped.js', function(root) {
-      root.result.skipped.length.should.eq(3);
+    run('skipped.js', function(result) {
+      result.failed.should.eq(0);
+      result.passed.should.eq(0);
       done();
     });
   });
 });
-
-function run(test, fn) {
-  var options = { tests: [fixtures + '/' + test] };
-  var hydro = new Hydro;
-
-  global.t = function() {
-    return hydro.addTest.apply(hydro, arguments);
-  };
-
-  global.s = function() {
-    return hydro.addSuite.apply(hydro, arguments);
-  };
-
-  hydro.run(options, function(res) {
-    global.t = null;
-    fn(res);
-  });
-}
---
+++ test/support/run.js
@@ -0,0 +1,27 @@
+var Hydro = require('../..');
+var fixtures = require('path').join(__dirname, '..', 'fixtures');
+
+module.exports = function(test, fn) {
+  var options = { tests: [fixtures + '/' + test] };
+  var hydro = new Hydro;
+  var result = { failed: 0, passed: 0 };
+
+  global.t = function() {
+    return hydro.addTest.apply(hydro, arguments);
+  };
+
+  global.s = function() {
+    return hydro.addSuite.apply(hydro, arguments);
+  };
+
+  hydro.on('post:test', function(test) {
+    if (test.skipped) return result.skipped++;
+    if (test.failed) return result.failed++;
+    result.passed++;
+  });
+
+  hydro.run(options, function() {
+    global.t = null;
+    fn(result);
+  });
+};
