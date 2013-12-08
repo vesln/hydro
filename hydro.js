e60a954f37e1720ddcbf2c8a822ba56e20ea18ec
Veselin Todorov <veselin@veselin.bg>
2013-12-08 13:02:21 +0200

    Add vesln/super

--- hydro.js
+++ hydro.js
@@ -265,6 +265,124 @@ EventEmitter.prototype.emit = function(evt, /* arg1, arg2 */ fn) {
 module.exports = EventEmitter;
 
 });
+require.register("vesln-super/lib/super.js", function(exports, require, module){
+/**
+ * slice
+ */
+
+var slice = Array.prototype.slice;
+
+/**
+ * Primary export
+ */
+
+var exports = module.exports = super_;
+
+/**
+ * ### _super (dest, orig)
+ *
+ * Inherits the prototype methods or merges objects.
+ * This is the primary export and it is recommended
+ * that it be imported as `inherits` in node to match
+ * the auto imported browser interface.
+ *
+ *     var inherits = require('super');
+ *
+ * @param {Object|Function} destination object
+ * @param {Object|Function} source object
+ * @name _super
+ * @api public
+ */
+
+function super_() {
+  var args = slice.call(arguments);
+  if (!args.length) return;
+  if (typeof args[0] !== 'function') return exports.merge(args);
+  exports.inherits.apply(null, args);
+};
+
+/**
+ * ### extend (proto[, klass])
+ *
+ * Provide `.extend` mechanism to allow extenion without
+ * needing to use dependancy.
+ *
+ *     function Bar () {
+ *       this._konstructed = true;
+ *     }
+ *
+ *     Bar.extend = inherits.extend;
+ *
+ *     var Fu = Bar.extend({
+ *       initialize: function () {
+ *         this._initialized = true;
+ *       }
+ *     });
+ *
+ *     var fu = new Fu();
+ *     fu.should.be.instanceof(Fu); // true
+ *     fu.should.be.instanceof(Bar); // true
+ *
+ * @param {Object} properties/methods to add to new prototype
+ * @param {Object} properties/methods to add to new class
+ * @returns {Object} new constructor
+ * @name extend
+ * @api public
+ */
+
+exports.extend = function(proto, klass) {
+  var self = this
+    , child = function () { return self.apply(this, arguments); };
+  exports.merge([ child, this ]);
+  exports.inherits(child, this);
+  if (proto) exports.merge([ child.prototype, proto ]);
+  if (klass) exports.merge([ child, klass ]);
+  child.extend = this.extend; // prevent overwrite
+  return child;
+};
+
+/**
+ * ### inherits (ctor, superCtor)
+ *
+ * Inherit the prototype methods from on contructor
+ * to another.
+ *
+ * @param {Function} destination
+ * @param {Function} source
+ * @api private
+ */
+
+exports.inherits = function(ctor, superCtor) {
+  ctor.super_ = superCtor;
+  ctor.prototype = Object.create(superCtor.prototype,
+    { constructor: {
+          value: ctor
+        , enumerable: false
+        , writable: true
+        , configurable: true
+      }
+  });
+}
+
+/**
+ * Extends multiple objects.
+ *
+ * @param {Array} array of objects
+ * @api private
+ */
+
+exports.merge = function (arr) {
+  var main = arr.length === 2 ? arr.shift() : {};
+  arr.forEach(function(obj) {
+    for (var p in obj) {
+      if (!obj.hasOwnProperty(p)) continue;
+      main[p] = obj[p];
+    }
+  });
+  return main;
+};
+
+});
 require.register("hydro/lib/hydro.js", function(exports, require, module){
 /**
  * External dependencies.
@@ -832,10 +950,18 @@ Suite.prototype.run = function(events, fn) {
 module.exports = Suite;
 
 });
+
+
+
+
 require.alias("vesln-evts/lib/evts.js", "hydro/deps/evts/lib/evts.js");
 require.alias("vesln-evts/lib/evts.js", "hydro/deps/evts/index.js");
 require.alias("vesln-evts/lib/evts.js", "evts/index.js");
 require.alias("vesln-evts/lib/evts.js", "vesln-evts/index.js");
+require.alias("vesln-super/lib/super.js", "hydro/deps/super/lib/super.js");
+require.alias("vesln-super/lib/super.js", "hydro/deps/super/index.js");
+require.alias("vesln-super/lib/super.js", "super/index.js");
+require.alias("vesln-super/lib/super.js", "vesln-super/index.js");
 require.alias("hydro/lib/hydro.js", "hydro/index.js");if (typeof exports == "object") {
   module.exports = require("hydro");
 } else if (typeof define == "function" && define.amd) {
