87551807b3854046361e369ac156a52238deaeaa
Veselin Todorov <veselin@veselin.bg>
2013-12-09 17:48:28 +0200

    Remove `use`, add plugins with option

--- test/hydro.plugins.js
+++ test/hydro.plugins.js
@@ -1,9 +1,7 @@
 t('plugin invocation', function(done) {
   var hydro = new Hydro;
-
-  hydro.use(plugin);
+  hydro.set('plugins', [plugin]);
   hydro.run();
-
   function plugin(_hydro) {
     _hydro.should.eq(hydro);
     done();
