0fc68497e640e904f3f403dc44432e65d40c5e2d
Veselin Todorov <veselin@veselin.bg>
2013-12-10 08:31:38 +0200

    Add more tests

--- test/hydro.addtest.js
+++ test/hydro.addtest.js
@@ -10,3 +10,16 @@ t('Runner delegation', function() {
   assert(runner.tests[0][0] === 'foo');
   assert(runner.tests[0][1] === 'bar');
 });
+
+t('adding a test without a suite', function() {
+  var hydro = new Hydro;
+  var err = null;
+
+  try {
+    hydro.addTest('foo', 'bar');
+  } catch(e) {
+    err = e;
+  }
+
+  assert(err.message === 'Please register a test suite');
+});
