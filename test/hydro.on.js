6fe95ece0a3243848f351b5d32cc6eb3737c5679
Veselin Todorov <veselin@veselin.bg>
2013-12-10 08:14:24 +0200

    Add tests for `Hydro#on`

---
+++ test/hydro.on.js
@@ -0,0 +1,34 @@
+t('subscribing to events', function(done) {
+  var fixtures = require('path').join(__dirname, 'support', 'fixtures');
+  var hydro = new Hydro;
+  var called = false;
+
+  hydro.on('pre:file', function() {
+    called = true;
+  });
+
+  hydro.push('tests', fixtures + '/blank.js');
+
+  hydro.run(function() {
+    assert(called);
+    done();
+  });
+
+}).skip(typeof window !== 'undefined');
+
+t('subscribing to events', function(done) {
+  var hydro = new Hydro;
+  var called = false;
+
+  hydro.on('pre:file', function() {
+    called = true;
+  });
+
+  hydro.push('tests', '../fixtures/blank.js');
+
+  hydro.run(function() {
+    assert(called);
+    done();
+  });
+
+}).skip(typeof window === 'undefined');
