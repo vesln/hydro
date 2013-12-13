1f35bc63b718a8a43c9e7cab45a7d71864ca9cce
Veselin Todorov <veselin@veselin.bg>
2013-12-13 17:56:57 +0200

    Allow adding a suite without a body

--- test/hydro.addsuite.js
+++ test/hydro.addsuite.js
@@ -1,5 +1,5 @@
 t('add suites', function() {
-  var hydro = new Hydro;
+  var hydro = Hydro();
 
   hydro.addSuite('suite 1', function() {
     hydro.addSuite('suite 2', function() {});
@@ -9,4 +9,18 @@ t('add suites', function() {
 
   assert(suites[1].title === 'suite 1');
   assert(suites[2].title === 'suite 2');
+});
+
+t('add suite without a bodu', function() {
+  var hydro = Hydro();
+
+  hydro.addSuite('suite 1');
+  hydro.addSuite('suite 2');
+
+  var suites = hydro.suites();
+
+  assert(suites[1].title === 'suite 1');
+  assert(typeof suites[1].parent.title === 'undefined');
+  assert(suites[2].title === 'suite 2');
+  assert(typeof suites[2].parent.title === 'undefined');
 });
