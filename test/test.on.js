fc66610d512f0681795699f76db2a06b0ea243cc
Jake Rosoman <jkroso@gmail.com>
2014-01-26 10:43:15 +1300

    add error handling in test[before/after] hooks

--- test/test.on.js
+++ test/test.on.js
@@ -1,3 +1,4 @@
+
 t('before', function(done) {
   var called = false;
   var hydro = new Hydro;
@@ -23,5 +24,43 @@ t('after', function(done) {
   test.run(hydro.emitter, function(){
     assert(called);
     done();
+  });
+});
+
+s('error handling', function() {
+  t('async before hook', function(done){
+    var hydro = new Hydro;
+    var test = new Hydro.Test.SyncTest('hooks', function() {});
+    var err = new Error('async before hook')
+    test.on('before', function(done){ done(err); });
+    test.run(hydro.emitter, function(error){
+      assert(error === err);
+      done();
+    });
+  });
+
+  t('async after hook', function(done){
+    var hydro = new Hydro;
+    var test = new Hydro.Test.SyncTest('hooks', function() {});
+    var err = new Error('async after hook')
+    test.on('after', function(done){ done(err); });
+    test.run(hydro.emitter, function(error){
+      assert(error === err);
+      done();
+    });
+  });
+
+  t('should propagate to the top', function(done) {
+    var hydro = new Hydro;
+    var err = new Error('propagate');
+    hydro.addSuite('suite', function(){
+      hydro.addTest('boom', function(){}).on('before', function(){
+        throw err;
+      });
+    });
+    hydro.run(function(error){
+      assert(error === err);
+      done();
+    });
   });
 });
