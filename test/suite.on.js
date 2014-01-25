79f473e8f502bb70b35202a453921203247cd30f
Jake Rosoman <jkroso@gmail.com>
2014-01-26 11:06:56 +1300

    add suite[before/after] hooks

---
+++ test/suite.on.js
@@ -0,0 +1,58 @@
+
+t('before', function(done) {
+  var called = false;
+  var hydro = new Hydro;
+  var suite = new Hydro.Suite('hooks');
+  suite.on('before', function(){ called = true; });
+  suite.run(hydro.emitter, function(){
+    assert(called);
+    done();
+  });
+});
+
+t('after', function(done) {
+  var called = false;
+  var hydro = new Hydro;
+  var suite = new Hydro.Suite('hooks');
+  suite.on('after', function(){ called = true; });
+  suite.run(hydro.emitter, function(){
+    assert(called);
+    done();
+  });
+});
+
+s('error handling', function() {
+  t('async before hook', function(done){
+    var hydro = new Hydro;
+    var suite = new Hydro.Suite('hooks');
+    var err = new Error('async before hook')
+    suite.on('before', function(done){ done(err); });
+    suite.run(hydro.emitter, function(error){
+      assert(error === err);
+      done();
+    });
+  });
+
+  t('async after hook', function(done){
+    var hydro = new Hydro;
+    var suite = new Hydro.Suite('hooks');
+    var err = new Error('async after hook')
+    suite.on('after', function(done){ done(err); });
+    suite.run(hydro.emitter, function(error){
+      assert(error === err);
+      done();
+    });
+  });
+
+  t('should propagate to the top', function(done) {
+    var hydro = new Hydro;
+    var err = new Error('propagate');
+    hydro.addSuite(function suite(){}).on('before', function(){
+      throw err;
+    });
+    hydro.run(function(error){
+      assert(error === err);
+      done();
+    });
+  });
+});
