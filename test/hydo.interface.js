450b1afcde882fbf981f10bee3465f13918f5863
Jake Rosoman <jkroso@gmail.com>
2014-01-26 13:22:46 +1300

    add interface tests

--- test/hydo.interface.js
+++ test/hydo.interface.js
@@ -27,5 +27,39 @@ t('afterNext', function(done) {
       done();
     });
   });
-  hydro.run(done);
+  hydro.run();
+});
+
+t('before', function(done) {
+  var hydro = new Hydro;
+  var count = 0;
+  hydro.addSuite('suite', function(){
+    hydro.interface.before(function() { count++; });
+    hydro.addTest('one', function() {
+      assert(count === 1);
+    });
+    hydro.addTest('two', function(){
+      assert(count === 2);
+    });
+    hydro.addSuite('inner suite', function() {
+      hydro.addTest('three', function(){
+        assert(count === 3);
+        done();
+      });
+    });
+  });
+  hydro.run();
+});
+
+t('beforeAll', function(done) {
+  var hydro = new Hydro;
+  var called = false;
+  hydro.addSuite('suite', function(){
+    hydro.addTest('one', function() {
+      assert(called);
+      done();
+    });
+    hydro.interface.beforeAll(function() { called = true; });
+  });
+  hydro.run();
 });
