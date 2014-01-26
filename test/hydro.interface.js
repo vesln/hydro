41da261d3ae1607f4c9e0ad8333ff6f91322dde5
Veselin Todorov <veselin@veselin.bg>
2014-01-26 09:54:25 +0200

    Coding style

--- test/hydro.interface.js
+++ test/hydro.interface.js
@@ -1,6 +1,7 @@
 t('beforeNext', function(done) {
   var hydro = new Hydro;
   var called = false;
+
   hydro.addSuite('suite', function() {
     hydro.interface.beforeNext(function() { called = true; });
     hydro.addTest('beforeNext', function(done) {
@@ -8,39 +9,48 @@ t('beforeNext', function(done) {
       done();
     });
   });
+
   hydro.run(done);
 });
 
 t('afterNext', function(done) {
   var hydro = new Hydro;
   var called = false;
+
   hydro.addSuite('suite', function(){
     hydro.interface.afterNext(function() {
       assert(!called, 'should not be called twice')
       called = true;
     });
+
     hydro.addTest('afterNext', function() {
       assert(!called);
     });
+
     hydro.addTest('another test', function(){
       assert(called);
       done();
     });
   });
+
   hydro.run();
 });
 
 t('before', function(done) {
   var hydro = new Hydro;
   var count = 0;
+
   hydro.addSuite('suite', function(){
     hydro.interface.before(function() { count++; });
+
     hydro.addTest('one', function() {
       assert(count === 1);
     });
+
     hydro.addTest('two', function(){
       assert(count === 2);
     });
+
     hydro.addSuite('inner suite', function() {
       hydro.addTest('three', function(){
         assert(count === 3);
@@ -48,12 +58,14 @@ t('before', function(done) {
       });
     });
   });
+
   hydro.run();
 });
 
 t('beforeAll', function(done) {
   var hydro = new Hydro;
   var called = false;
+
   hydro.addSuite('suite', function(){
     hydro.addTest('one', function() {
       assert(called);
@@ -61,5 +73,6 @@ t('beforeAll', function(done) {
     });
     hydro.interface.beforeAll(function() { called = true; });
   });
+
   hydro.run();
 });
