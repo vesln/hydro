6c07cae3799ae4b0512eb714e3e2cf15fbdf249f
Jake Rosoman <jkroso@gmail.com>
2014-01-27 15:56:02 +1300

    make sure errors get thrown if not handled

--- test/hydro.run.js
+++ test/hydro.run.js
@@ -1,3 +1,36 @@
+
+var tryc = require('tryc');
+
 t('no params', function() {
   Hydro().run();
+});
+
+t('error handling', function(done) {
+  var hydro = new Hydro;
+
+  hydro.on('pre:all', function() {
+    throw new Error('boom');
+  });
+
+  hydro.run(function(err) {
+    assert(err != null);
+    assert(err.message == 'boom');
+    done();
+  });
+});
+
+t('throw error if there is no callback', function(done) {
+  var hydro = new Hydro;
+
+  hydro.on('pre:all', function() {
+    throw new Error('boom');
+  });
+
+  tryc(function(){
+    hydro.run();
+  }, function(err){
+    assert(err instanceof Error);
+    assert(err.message == 'boom');
+    done();
+  });
 });
